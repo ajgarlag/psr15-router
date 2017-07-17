c8af408036ea0c71af22f426c8667a09e5031dce
Antonio J. García Lagar <aj@garcialagar.es>
2017-07-17 12:39:20 +0200

    Adds a request matcher to match any request

---
+++ src/Matcher/AlwaysMatchRequestMatcher.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * PSR-15 Router by @ajgarlag
+ *
+ * Copyright (C) Antonio J. García Lagar <aj@garcialagar.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ajgarlag\Psr15\Router\Matcher;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+class AlwaysMatchRequestMatcher implements RequestMatcher
+{
+    public function match(ServerRequestInterface $request)
+    {
+        return true;
+    }
+}
